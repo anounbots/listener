4dfcb8e8bbabf84171b3e18c744290651836bfcc
Anoun <pertoli@mail.com>
2017-08-02 10:28:55 +0100

    Update lizard.js

--- src/commands/lizard.js
+++ src/commands/lizard.js
@@ -1,5 +1,7 @@
 const snekfetch = require('snekfetch');
 exports.run = (list, msg, args, logger) => {
+    list.stats.lizard++;
+    list.db(list.stats);
 snekfetch.get('https://nekos.life/api/lizard')
         .then(r => msg.channel.send({
             embed: {
