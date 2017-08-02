9ebe5239b10dc883ed55ba9e89427a1d3c3ec9cf
Anoun <pertoli@mail.com>
2017-08-02 10:27:11 +0100

    Update list.js

--- src/list.js
+++ src/list.js
@@ -6,6 +6,7 @@ const path = require('path');
 const dbPath = path.resolve(__dirname, '..', 'database', 'db.sqlite');
 const sql = require('sqlite');
 sql.open(dbPath);
+require("./util/utils.js")(list);
 
 list.on('ready', () => {
     list.user.setGame(`tlhelp | ${list.guilds.size} servers!`);
