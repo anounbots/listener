86563a2c7885d82df6ca5a5f2934d779a7bf8f11
Alshaikh <u.a.esdf@hotmail.com>
2017-08-02 04:37:20 +0400

    added set nsfw

--- src/commands/set.js
+++ src/commands/set.js
@@ -1,27 +1,54 @@
 const path = require('path');
-const dbPath = path.resolve(__dirname, '..', 'database', 'db.sqlite')
+const dbPath = path.resolve(__dirname, '..', 'database', 'db.sqlite');
 const sql = require('sqlite');
 sql.open(dbPath);
 exports.run = (list, msg, args, logger) => {
-    let feature = args[0]
-    let to = args.slice(1).join(' ')
+    let feature = args[0];
+    let to = args.slice(1).join(' ');
     if (feature === 'prefix') {
         sql.get(`SELECT * FROM guilds WHERE guildID ='${msg.guild.id}'`).then(guild => {
-        if (!guild) {
-            sql.run('INSERT INTO guilds (guildID, prefix, ownerID) VALUES (?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id])
-            msg.channel.send('**Inserting this guild into the database!**')
-            msg.delete(1500)
-        } else {
-                if (!to) return msg.reply('You must specify a prefix!')
-                sql.run(`UPDATE guilds SET prefix ='${to}' WHERE guildID ='${msg.guild.id}'`)
-                msg.reply(`:white_check_mark: Prefix was succefully changed to **${to}**`) 
-        }
-    }).catch(() => {
-        logger.error();
-        sql.run("CREATE TABLE IF NOT EXISTS guilds (guildID TEXT, prefix TEXT, ownerID TEXT)").then(() => {
-            sql.run('INSERT INTO guilds (guildID, prefix, ownerID) VALUES (?, ?, ?)', [msg.guild.id, to, msg.guild.owner.id])
-            msg.reply(':white_check_mark: Success! You have changed the prefix!')
+            if (!guild) {
+                sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES (?, ?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id, null]);
+                msg.channel.send('**Inserting this guild into the database!**');
+                msg.delete(1500);
+            } else {
+                if (!to) return msg.reply('You must specify a prefix!');
+                sql.run(`UPDATE guilds SET prefix ='${to}' WHERE guildID ='${msg.guild.id}'`);
+                msg.reply(`:white_check_mark: Prefix was succefully changed to **${to}**`);
+            }
+        }).catch(() => {
+            logger.error();
+            sql.run('CREATE TABLE IF NOT EXISTS guilds (guildID TEXT, prefix TEXT, ownerID TEXT, nsfwID TEXT)').then(() => {
+                sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES (?, ?, ?, ?)', [msg.guild.id, to, msg.guild.owner.id, null]);
+                msg.reply(':white_check_mark: Success! You have changed the prefix!');
+            });
         });
-    });
-    } 
+    } else if (feature === 'nsfw') {
+        if (!msg.channel.permissionsFor(list.user).has('MANAGE_CHANNELS')) {
+            return msg.reply('I do not have permission to set this channel to nsfw \\:(');
+        }
+        sql.get(`SELECT * FROM guilds WHERE guildID = '${msg.guild.id}'`).then(guild => {
+            if (!guild) {
+                if (!msg.channel.nsfw) {
+                    msg.channel.edit({ nsfw: true });
+                    sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES(?, ?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id, msg.channel.id]);
+                    msg.reply(':white_check_mark: Success! You have set this channel to NSFW');
+                } else {
+                    sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES(?, ?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id, msg.channel.id]);
+                    msg.reply(':white_check_mark: Success! You have set this channel to NSFW');
+                }
+            } else {
+                if (!msg.channel.nsfw) msg.channel.edit({ nsfw: true });
+                sql.run(`UPDATE guilds SET nsfwID = '${msg.channel.id} WHERE guildID ='${msg.guild.id}`);
+                msg.reply(':white_check_mark: You have successfully set this channel to nsfw!');
+            }
+        }).catch(() => {
+            logger.error();
+            sql.run('CREATE TABLE IF NOT EXISTS guilds (guildID TEXT, prefix TEXT, ownerID TEXT, nsfwID TEXT)').then(() => {
+                if (!msg.channel.nsfw) msg.channel.edit({ nsfw: true });
+                sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES (?, ?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id, msg.channel.id]);
+                msg.reply(':white_check_mark: Success! You have set this channel to NSFW');
+            });
+        });
+    }
 };
--- src/list.js
+++ src/list.js
@@ -3,53 +3,54 @@ const list = new Client();
 const logger = require('./util/Logger.js');
 const config = require('./config/config.json');
 const path = require('path');
-const dbPath = path.resolve(__dirname, '..', 'database', 'db.sqlite')
+const dbPath = path.resolve(__dirname, '..', 'database', 'db.sqlite');
 const sql = require('sqlite');
 sql.open(dbPath);
 
 list.on('ready', () => {
-    list.user.setGame(`tlhelp | ${list.guilds.size} servers!`)
-    logger.info('3')
-    logger.info('2')
-    logger.info('1')
-    logger.info(`Listener is ready on ${list.guilds.size} servers with ${list.users.size} users!`)
+    list.user.setGame(`tlhelp | ${list.guilds.size} servers!`);
+    logger.info('3');
+    logger.info('2');
+    logger.info('1');
+    logger.info(`Listener is ready on ${list.guilds.size} servers with ${list.users.size} users!`);
 });
 
 list.on('message', async(msg) => {
     sql.get(`SELECT * FROM guilds WHERE guildID ='${msg.guild.id}'`).then(guild => {
         if (!guild) {
-            sql.run('INSERT INTO guilds (guildID, prefix, ownerID) VALUES (?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id])
+            sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES (?, ?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id, null]);
         } else {
             if (msg.author.bot) return;
             if (!msg.content.startsWith(guild.prefix)) return;
 
-            const args = msg.content.split(" ");
+            const args = msg.content.split(' ');
             const command = args.shift().slice(guild.prefix.length);
 
             try {
-                let commandFile = require(`./commands/${command}.js`)
-                commandFile.run(list, msg, args, logger)
+                let commandFile = require(`./commands/${command}.js`);
+                commandFile.run(list, msg, args, logger);
             } catch (err) {
-                logger.error(err)
-                msg.channel.send({ embed: { color: config.embedColor, title: err.stack.split('\n')[0], description: e.message + "\n" + e.stack.split("\n")[1] + '\n Please, go over to: so AnounFX™#3494 can fix error'}})
+                if (err.stack.startsWith('Error: Cannot find module')) return;
+                logger.error(err);
+                msg.channel.send({ embed: { color: config.embedColor, title: err.stack.split('\n')[0], description: err.message + '\n' + err.stack.split('\n')[1] + '\n Please, go over to: so AnounFX™#3494 can fix error' } });
             }
         }
     }).catch(() => {
         logger.error();
-        sql.run("CREATE TABLE IF NOT EXISTS guilds (guildID TEXT, prefix TEXT, ownerID TEXT)").then(() => {
-            sql.run('INSERT INTO guilds (guildID, prefix, ownerID) VALUES (?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id])
+        sql.run('CREATE TABLE IF NOT EXISTS guilds (guildID TEXT, prefix TEXT, ownerID TEXT, nsfwID TEXT)').then(() => {
+            sql.run('INSERT INTO guilds (guildID, prefix, ownerID, nsfwID) VALUES (?, ?, ?, ?)', [msg.guild.id, 'tl', msg.guild.owner.id, null]);
         });
     });
 });
 
 list.on('warn', (e) => {
-    logger.warn(e)
+    logger.warn(e);
 });
 list.on('error', (e) => {
-    logger.error(e)
+    logger.error(e);
 });
 list.on('debug', (e) => {
-    logger.info(e)
+    logger.info(e);
 });
 
 list.login(config.tokens.discord);
