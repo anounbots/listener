3df44ba463cc01f5ebdaab8065a558bca3ec5640
Anoun <pertoli@mail.com>
2017-08-02 10:21:43 +0100

    Create lizard.js

---
+++ src/commands/lizard.js
@@ -0,0 +1,15 @@
+const snekfetch = require('snekfetch');
+exports.run = (list, msg, args, logger) => {
+snekfetch.get('https://nekos.life/api/lizard')
+        .then(r => msg.channel.send({
+            embed: {
+                color: c
+                author: {
+                    name: "Beautiful Lizard! \\o/",
+                    icon_url: list.user.avatarURL
+                },
+                image: {
+                    url: r.body.url
+                }
+            }
+        }).catch(e => logger.warn(e)));
