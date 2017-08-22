2301e9a9b7b675d639b8c9b73bd2a93d0c85f21d
Alshaikh <u.a.esdf@hotmail.com>
2017-08-23 03:44:15 +0400

    fixed dog command

--- src/commands/dog.js
+++ src/commands/dog.js
@@ -4,33 +4,33 @@ exports.run = (list, msg, args, logger) => {
     list.stats.dog++;
     list.db(list.stats);
     superagent.get('http://random.dog/woof.json')
-      .type('application/json')
-      .end((error, response) => {
-        if (error) {
-          logger.warn(error);
-          return msg.channel.send(`:x: | An error has occured!\`\`\`${error}\`\`\``);
-        }
-        return msg.channel.send({
-          embed: {
-            color: 0x008000,
-            author: {
-              name: 'Lovely dogs :3',
-              icon_url: list.user.avatarURL
-            },
-            image: { url: response.body.file }
-          }
+        .type('application/json')
+        .end((error, response) => {
+            if (error) {
+                logger.warn(error);
+                return msg.channel.send(`:x: | An error has occured!\`\`\`${error}\`\`\``);
+            }
+            return msg.channel.send({
+                embed: {
+                    color: 0x008000,
+                    author: {
+                        name: 'Lovely dogs :3',
+                        icon_url: list.user.avatarURL
+                    },
+                    image: { url: response.body.url }
+                }
+            });
         });
-      });
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['woof']
-  };
+    enabled: true,
+    guildOnly: false,
+    aliases: ['woof']
+};
 
-  exports.help = {
-  name: 'dog',
-  description: 'sends a random dog from http://random.dog/',
-  usage: '<prefix>dog'
-  };
+exports.help = {
+    name: 'dog',
+    description: 'sends a random dog from http://random.dog/',
+    usage: '<prefix>dog'
+};
